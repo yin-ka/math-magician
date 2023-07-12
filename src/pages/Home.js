b285654cf193232c8f3acf572a7844cbb396cf58
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-12 07:04:01 +0100

    Add contents to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h4>Welcome to our page!</h4>
+      <p>
+        Soon oft he and vexed unto are wassailers feere little.
+        Sad in had mirthful the was few, into but basked of grief and mothernot and,
+        below my childe consecrate shun.
+        But maddest sad thy have cared memory. Mammon through rhyme.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+        optio, eaque rerum! Provident similique accusantium nemo autem.
+      </p>
+    </div>
   );
 }
 
