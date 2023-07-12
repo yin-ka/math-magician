7e00fb97ae60c11318b9aca4e419f7e1bd346e57
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-12 09:58:30 +0100

    fixed server error

--- src/pages/MyCalculator.js
+++ src/pages/MyCalculator.js
@@ -4,7 +4,7 @@ import Calculator from '../components/Calculator';
 function MyCalculator() {
   return (
     <div>
-      <h4>Let us do some math!</h4>
+      <h4>Let&apos;s do some math!</h4>
       <Calculator />
     </div>
   );
