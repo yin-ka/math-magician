ad3faa8f7a6830f0d1059b6263f27f4cb9c53ef3
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-12 06:56:49 +0100

    Create Navbar.js file

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2 className="logo">Math Magicians</h2>
+      <ul className="nav-item">
+        <li className="nav-item-list">Home</li>
+        <li className="nav-item-list">Calculator</li>
+        <li className="nav-item-list">Quote</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
