9de3d817ff1fea06a3b26ceb09d4f3d365b391fb
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-07-12 10:59:16 +0100

    fixed errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function Navbar({ home, calculator, quote }) {
+  Navbar.propTypes = {
+    home: PropTypes.string.isRequired,
+    calculator: PropTypes.string.isRequired,
+    quote: PropTypes.string.isRequired,
+  };
   return (
     <nav>
       <h2 className="logo">Math Magicians</h2>
       <ul className="nav-item">
-        <li className="nav-item-list"><Link to={home}>Home</Link></li>
-        <li className="nav-item-list"><Link to={calculator}>Calculator</Link></li>
-        <li className="nav-item-list"><Link to={quote}>Quote</Link></li>
+        <li className="nav-item-list"><NavLink to={home}>Home</NavLink></li>
+        <li className="nav-item-list"><NavLink to={calculator}>Calculator</NavLink></li>
+        <li className="nav-item-list"><NavLink to={quote}>Quote</NavLink></li>
       </ul>
     </nav>
   );
